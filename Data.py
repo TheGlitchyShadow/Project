1f32f42fff91859c39fefdfdfadbf2e26e2ac03f
TheGlitchyShadow <abnkipn@gmail.com>
2026-06-12 16:53:38 -0400

    Add files via upload

---
+++ Data.py
@@ -0,0 +1,14 @@
+s = "chips"
+p = 1.50
+q = 10
+a = True
+print(s,p,q,a)
+print(type(s), type(p), type(q), type(a))
+t = p*q
+print(t)
+print(p - 0.25)
+print(q*2)
+print(p<2)
+print(q>5)
+print(p == 1.50)
+s1 = 
