3859b86dc923b228a857bfc8028a333dd6db0875
TheGlitchyShadow <abnkipn@gmail.com>
2026-09-18 16:56:24 -0400

    Add files via upload

---
+++ Opps2.1.py
@@ -0,0 +1,9 @@
+class Playlist:
+
+    def __init__(self, name, genre):
+        self.name = name
+        self.genre = genre
+        self.songs = []
+        print(f"Playlist '{self.name}' ({self.genre}) is ready!")
+
+    def add_song(self
